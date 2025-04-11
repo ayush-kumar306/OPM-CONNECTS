fd1314962b42bba48e217d79fcecad099eba943b
AYUSH KUMAR <opm.ayushkumar@gmail.com>
2025-04-11 14:08:25 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -17,7 +17,12 @@ const Contact = () => {
           <p className=' text-gray-500'>Mob.: +91 9142399702 <br /> Email: opmconnects@gmail.com</p>
           <p className=' font-semibold text-lg text-gray-600'>CAREERS AT OPM CONNECTS</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
-          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'><a 
+    href="https://mail.google.com/mail/?view=cm&to=opmconnects@gmail.com" 
+    target="_blank" 
+    rel="noopener noreferrer" 
+    className="hover:underline">
+Explore Jobs</a></button>
         </div>
       </div>
 
