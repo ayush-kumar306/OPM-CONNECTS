3d9bc920dcb80854e01085031d41ca80ab98f184
AYUSH KUMAR <opm.ayushkumar@gmail.com>
2025-04-12 03:41:38 +0530

    Update DoctorDashboard.jsx

--- admin/src/pages/Doctor/DoctorDashboard.jsx
+++ admin/src/pages/Doctor/DoctorDashboard.jsx
@@ -31,7 +31,7 @@ const DoctorDashboard = () => {
           </div>
         </div>
         <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
-          <img className='w-14' src={assets.appointments_icon} alt="" />
+          <img className='w-14 h-14' src={assets.appointments_icon} alt="" />
           <div>
             <p className='text-xl font-semibold text-gray-600'>{dashData.appointments}</p>
             <p className='text-gray-400'>Appointments</p>
