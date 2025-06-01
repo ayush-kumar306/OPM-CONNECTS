005c83d0486a2b10c7a0812080ef043ee3dc1a7d
AYUSH KUMAR <opm.ayushkumar@gmail.com>
2025-06-01 06:26:50 +0530

    Update doctorRoute.js

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { loginDoctor, appointmentsDoctor, appointmentCancel, doctorList, changeAvailablity, appointmentComplete, doctorDashboard, doctorProfile, updateDoctorProfile } from '../controllers/doctorController.js';
 import authDoctor from '../middleware/authDoctor.js';
+import { bookAppointmentDoctor } from "../controllers/doctorController.js";
 const doctorRouter = express.Router();
 
 doctorRouter.post("/login", loginDoctor)
@@ -12,5 +13,6 @@ doctorRouter.post("/complete-appointment", authDoctor, appointmentComplete)
 doctorRouter.get("/dashboard", authDoctor, doctorDashboard)
 doctorRouter.get("/profile", authDoctor, doctorProfile)
 doctorRouter.post("/update-profile", authDoctor, updateDoctorProfile)
+doctorRouter.post("/book-appointment", authDoctor, bookAppointmentDoctor);
 
 export default doctorRouter;
